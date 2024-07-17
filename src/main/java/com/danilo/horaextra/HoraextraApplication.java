07fd181245f41b1339bcb690c86ad153f76f3d19
Danilo <danilodiniz93@yahoo.com>
2024-07-17 13:48:51 -0300

    projeto hora extra criado

---
+++ src/main/java/com/danilo/horaextra/HoraextraApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.horaextra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraextraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraextraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/horaextra/HoraextraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.horaextra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraextraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
